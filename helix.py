d8740cc37f66ca57c1efc92e9660073f7880fab8
Michael Kessler <mikepkes+git@gmail.com>
2023-02-11 22:19:52 -0800

    Provide final clearing fixes.

--- helix.py
+++ helix.py
@@ -10,17 +10,33 @@ def helix_plunge(cmd, x, y, z, depth, feed, step, radius, tool_diameter=None):
     else:
         radius_c = radius
 
-    # Move to the start position.
-    cmds = f"G01 X{x-radius_c} Y{y} Z{z}\n"
+    # Move to the start position.  This could probably be a rapid.
+    cmds = f"G01 X{x-radius_c} Y{y} Z{z} F{feed}\n"
     zpos = z
-    opp = False
+    opp = True
     while zpos > z-depth:
         zpos = max(zpos-(step*.5), z-depth)
         if opp:
             cmds += f"{cmd} X{x+radius_c} I{radius_c} Z{zpos} F{feed}\n"
         else:
-            cmds += f"{cmd} X{x-radius_c} I{radius_c} Z{zpos} F{feed}\n"
+            cmds += f"{cmd} X{x-radius_c} I{-radius_c} Z{zpos} F{feed}\n"
         opp = not opp
+
+    #Finish the bottom circle at full depth.
+    if opp:
+        cmds += f"{cmd} X{x+radius_c} I{radius_c} Z{zpos} F{feed}\n"
+    else:
+        cmds += f"{cmd} X{x-radius_c} I{-radius_c} Z{zpos} F{feed}\n"
+    opp = not opp
+
+    #Rotate back into center while rising a step.
+    if opp:
+        cmds += f"{cmd} X{x} I{radius_c*.5} Z{min(zpos+step, z)} F{feed}\n"
+    else:
+        cmds += f"{cmd} X{x} I{-radius_c*.5} Z{min(zpos+step, z)} F{feed}\n"
+
+    # Return to initial start.
+    cmds += f"G01 Z{z} F{feed}"
 
     return cmds
 
@@ -35,7 +51,7 @@ if __name__ == "__main__":
     parser.add_argument("-f", "--feed", type=float, help="feed", required=True)
     parser.add_argument("-s", "--step", type=float, help="step", required=True)
     parser.add_argument("-r", "--radius", type=float, help="radius", required=True)
-    parser.add_argument("-c", "--command", type=str, choices=["G02", "G03"], default="G02", help="Command, either G02 or G03, default G02")
+    parser.add_argument("-c", "--command", type=str, choices=["G02", "G03"], default="G02", help="Command, either G02 (clockwise) or G03 (counter-clockwise), default G02")
     parser.add_argument("-td", "--tool_diameter", type=float, default=None, help="Tool diameter")
 
     args = parser.parse_args()
