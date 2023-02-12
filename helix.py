5900ccab5d9f17a7a0a3990fdd59d358c68a3f93
Michael Kessler <mikepkes+git@gmail.com>
2023-02-11 17:25:17 -0800

    Add half movements so it doesn't rely on 360 degree movements Add -c for specifying the command to be used.

--- helix.py
+++ helix.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
 
-def helix_plunge(x, y, z, depth, feed, step, radius):
+def helix_plunge(cmd, x, y, z, depth, feed, step, radius):
     cmds = f"G01 X{x-radius} Y{y} Z{z}\n"
     zpos = z
+    opp = False
     while zpos > z-depth:
-        zpos = max(zpos-step, z-depth)
-        cmds += f"G02 X{x-radius} I{radius} Z{zpos} F{feed}\n"
+        zpos = max(zpos-(step*.5), z-depth)
+        if opp:
+            cmds += f"{cmd} X{x+radius} I{radius} Z{zpos} F{feed}\n"
+        else:
+            cmds += f"{cmd} X{x-radius} I{radius} Z{zpos} F{feed}\n"
+        opp = not opp
+
     return cmds
 
 if __name__ == "__main__":
@@ -19,10 +25,12 @@ if __name__ == "__main__":
     parser.add_argument("-f", "--feed", type=float, help="feed", required=True)
     parser.add_argument("-s", "--step", type=float, help="step", required=True)
     parser.add_argument("-r", "--radius", type=float, help="radius", required=True)
+    parser.add_argument("-c", "--command", type=str, default="G02", help="Command, either G02 or G03, default G02")
 
     args = parser.parse_args()
 
     print(helix_plunge(
+        args.command,
         args.x,
         args.y,
         args.z,
